3a71c59fd1fed47b8a50f237d6fe6e1443ea45ee
aniruddh-r-sharma <aniruddh.r.sharma@gmail.com>
2020-08-09 11:47:16 +0100

    google auth

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -3,6 +3,7 @@ import 'package:terawe_flutter_project/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:terawe_flutter_project/screens/products_screen.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
@@ -11,9 +12,38 @@ class LoginScreen extends StatefulWidget {
 
 class _LoginScreenState extends State<LoginScreen> {
   final _auth = FirebaseAuth.instance;
+  final GoogleSignIn googleSignIn = GoogleSignIn();
 
   String email;
   String password;
+
+  Future<String> signInWithGoogle() async {
+    final GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+    final GoogleSignInAuthentication googleSignInAuthentication =
+        await googleSignInAccount.authentication;
+
+    final AuthCredential credential = GoogleAuthProvider.getCredential(
+      accessToken: googleSignInAuthentication.accessToken,
+      idToken: googleSignInAuthentication.idToken,
+    );
+
+    final AuthResult authResult = await _auth.signInWithCredential(credential);
+    final FirebaseUser user = authResult.user;
+
+    assert(!user.isAnonymous);
+    assert(await user.getIdToken() != null);
+
+    final FirebaseUser currentUser = await _auth.currentUser();
+    assert(user.uid == currentUser.uid);
+
+    return 'signInWithGoogle succeeded: $user';
+  }
+
+  void signOutGoogle() async {
+    await googleSignIn.signOut();
+
+    print("User Sign Out");
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -221,7 +251,15 @@ class _LoginScreenState extends State<LoginScreen> {
                     ),
                     child: IconButton(
                       onPressed: () {
-                        print('google button');
+                        signInWithGoogle().whenComplete(() {
+                          Navigator.of(context).push(
+                            MaterialPageRoute(
+                              builder: (context) {
+                                return ProductsScreen();
+                              },
+                            ),
+                          );
+                        });
                       },
                       icon: FaIcon(
                         FontAwesomeIcons.google,
